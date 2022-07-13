7a344dff167ebcf27f0d6ad43efa7e97f1fb6392
WaterLoggedBeans <109230784+WaterLoggedBeans@users.noreply.github.com>
2022-07-13 10:02:09 -0500

    Add files via upload

---
+++ Testproject1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproject1;
+
+/**
+ *
+ * @author George
+ */
+public class Testproject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a = 2, b = 14, c = 8;
+    double w = 14.6 , y = 2.7;
+    System.out.println(w % y);
+
+    }
+    
+}
